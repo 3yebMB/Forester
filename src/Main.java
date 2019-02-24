d1c335759dd51228d9acf2cf1c88710f3a193b12
3yebmb <3yebmb@gmail.com>
2019-02-25 02:12:47 +0500

    updated main class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
+    public static void main(String[] args) {
+        final String FILE_NAME = "forest.txt";
+        File file = new File(FILE_NAME);
+        char[] forest = new char[(int) file.length()];
+
+        try {
+            FileReader fr = new FileReader(file);
+            fr.read(forest);
+//            System.out.println(Arrays.toString(forest));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
