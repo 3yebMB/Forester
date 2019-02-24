69117ce665fe67dd3b1690c3fb38f86d7dbe7eb4
3yebmb <3yebmb@gmail.com>
2019-02-25 02:12:26 +0500

    updated counter methods

--- src/Counter_m1.java
+++ src/Counter_m1.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Counter_m1 {
+    private char[] input;
+
+    public Counter_m1(char[] input) {
+        this.input = input;
+    }
+
+    public void info() {
+        System.out.println("");
+    }
+
+    private String getTreeName(int i) {
+        String name = null;
+        switch (i) {
+            case 1 : name = "Ель"; break;
+            case 2 : name = "Сосна"; break;
+            case 3 : name = "Кедр"; break;
+            case 4 : name = "Берёза"; break;
+            case 5 : name = "Осина"; break;
+            case 379 : name = "Пальма"; break;
+            default: name = "Неизвестное дерево";
+        }
+        return name;
+    }
 }
--- src/Counter_m2.java
+++ src/Counter_m2.java
@@ -1,4 +1,2 @@
-package PACKAGE_NAME;
-
 public class Counter_m2 {
 }
