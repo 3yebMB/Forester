b48d9c3c3be5f26a4cb9722960279373eb76e921
3yebMB <3yebmb@gmail.com>
2019-02-25 11:57:14 +0500

    counter of trees has been done

--- src/Counter_m1.java
+++ src/Counter_m1.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Counter_m1 {
     private char[] input;
 
@@ -6,7 +9,19 @@ public class Counter_m1 {
     }
 
     public void info() {
-        System.out.println("");
+        String str = new String(input);
+        String[] tmp = str.split("\r\n |\r\n| "); // разбиваем лес, на отдельные деревья
+        Map<String, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < tmp.length; i++) {
+            Integer current = hm.get(tmp[i]);
+            hm.put(tmp[i], current == null ? 1 : current + 1);
+        } // подсчитываем кол-во деревьев
+
+        System.out.println("Лесник обошёл лес и подсчитал все деревья :");
+        for (Map.Entry<String, Integer> entry : hm.entrySet()) {
+            System.out.printf("\t%s - %d\r\n", getTreeName(Integer.valueOf(entry.getKey())), entry.getValue());
+        }
     }
 
     private String getTreeName(int i) {
