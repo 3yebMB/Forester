023a72b95475f1f8664051908305ebfecbde2e7e
3yebMB <3yebmb@gmail.com>
2019-02-25 12:43:32 +0500

    updated and renamed counter

--- src/Counter.java
+++ src/Counter.java
@@ -1,26 +1,26 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Counter_m1 {
+public class Counter {
     private char[] input;
 
-    public Counter_m1(char[] input) {
+    public Counter(char[] input) {
         this.input = input;
     }
 
     public void info() {
         String str = new String(input);
         String[] tmp = str.split("\r\n |\r\n| "); // разбиваем лес, на отдельные деревья
-        Map<String, Integer> hm = new HashMap<>();
+        Map<Integer, Integer> hm = new HashMap<>();
 
         for (int i = 0; i < tmp.length; i++) {
-            Integer current = hm.get(tmp[i]);
-            hm.put(tmp[i], current == null ? 1 : current + 1);
+            Integer current = hm.get(Integer.valueOf(tmp[i]));
+            hm.put(Integer.valueOf(tmp[i]), current == null ? 1 : current + 1);
         } // подсчитываем кол-во деревьев
 
         System.out.println("Лесник обошёл лес и подсчитал все деревья :");
-        for (Map.Entry<String, Integer> entry : hm.entrySet()) {
-            System.out.printf("\t%s - %d\r\n", getTreeName(Integer.valueOf(entry.getKey())), entry.getValue());
+        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+            System.out.printf("\t%s (%d) - %d\r\n", getTreeName(entry.getKey()), entry.getKey(), entry.getValue());
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,7 +12,7 @@ public class Main {
         try {
             FileReader fr = new FileReader(file);
             fr.read(forest);
-            Counter_m1 m1 = new Counter_m1(forest);
+            Counter m1 = new Counter(forest);
             m1.info();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
